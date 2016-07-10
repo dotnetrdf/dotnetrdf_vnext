06e5d92fa40b3dd5f2096263b0ab9ee7fb15f4c2
Kal Ahmed <kal@brightstardb.com>
2016-07-10 17:30:20 +0100

    Created skeleton project for dotNetRDF.IO.Html

---
+++ src/dotNetRDF.IO.Html/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("dotNetRDF.IO.Html")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("88dec606-5b83-4fd0-a42a-01a1ad9d3b9c")]
